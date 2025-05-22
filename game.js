ec067bcd86a9f9bb326a25d75350c6a4c3e0734a
anas1306 <mohammed.anas1306@gmail.com>
2025-05-23 02:02:36 +0300

    Made replayable

--- game.js
+++ game.js
@@ -93,69 +93,96 @@
     return winner;
 }
 
+function win() {
+    const endText = document.createElement("div");
+    endText.textContent = "You won the game!! Congratulations.";
+    endText.style.backgroundColor = "green";
+    endText.style.fontSize = "30px"
+    i = box.children.length
+    while (i > 1) {
+        box.removeChild(box.lastElementChild)
+        i -= 1;
+    }
+    box.appendChild(endText)
+}
+
+function lose() {
+    const endText = document.createElement("div");
+    endText.textContent = "You lost the game!! Boohoo.";
+    endText.style.backgroundColor = "red";
+    endText.style.fontSize = "30px"
+    i = box.children.length
+    while (i > 1) {
+        box.removeChild(box.lastElementChild)
+        i -= 1;
+    }
+    box.appendChild(endText)
+}
+
 const box = document.querySelector(".resultBox");
 
 const rock = document.querySelector("#choice1");
 const paper = document.querySelector("#choice2");
 const scissors = document.querySelector("#choice3");
+let won = 0;
+let lost = 0;
 
 rock.addEventListener("click", () => {
-
-    const result = playRound(`${rock.textContent}`, getComputerChoice());
-
-    const resultText = document.createElement("div");
-    resultText.textContent = result;
-
-    if (result === "You win! Rock beats Scissors.") {
-        resultText.classList.add("win")
-        box.appendChild(resultText)
-    } else if (result === "You lose! Paper beats Rock.") {
-        resultText.classList.add("lose")
-        box.appendChild(resultText)
+    if (won === 5) {
+        win()
+        won = 0;
+    } else if (lost === 5) {
+        lose()
+        lost = 0;
     } else {
-        resultText.classList.add("draw")
-        box.appendChild(resultText)
+        const result = playRound(`${rock.textContent}`, getComputerChoice());
+        const resultText = document.createElement("div");
+        resultText.textContent = result;
+        if (result === "You win! Rock beats Scissors.") {
+            resultText.classList.add("win");
+            box.appendChild(resultText);
+            won = won + 1;
+        } else if (result === "You lose! Paper beats Rock.") {
+            resultText.classList.add("lose")
+            box.appendChild(resultText)
+            lost = lost + 1;
+        } else {
+            resultText.classList.add("draw");
+            box.appendChild(resultText);
+        }
     }
-
 })
-
 paper.addEventListener("click", () => {
-
     const result = playRound(`${paper.textContent}`, getComputerChoice());
-
     const resultText = document.createElement("div");
     resultText.textContent = result;
-
     if (result === "You win! Paper beats Rock.") {
-        resultText.classList.add("win")
-        box.appendChild(resultText)
+        resultText.classList.add("win");
+        box.appendChild(resultText);
+        won = won + 1;
     } else if (result === "You lose! Scissors beats Paper.") {
-        resultText.classList.add("lose")
-        box.appendChild(resultText)
+        resultText.classList.add("lose");
+        box.appendChild(resultText);
+        lost = lost + 1;
     } else {
-        resultText.classList.add("draw")
-        box.appendChild(resultText)
+        resultText.classList.add("draw");
+        box.appendChild(resultText);
     }
-
 })
-
 scissors.addEventListener("click", () => {
-    
     const result = playRound(`${scissors.textContent}`, getComputerChoice());
-
     const resultText = document.createElement("div");
     resultText.textContent = result;
-
     if (result === "You win! Scissors beats Paper.") {
-        resultText.classList.add("win")
-        box.appendChild(resultText)
+        resultText.classList.add("win");
+        box.appendChild(resultText);
+        won = won + 1;
     } else if (result === "You lose! Rock beats Scissors.") {
-        resultText.classList.add("lose")
-        box.appendChild(resultText)
+        resultText.classList.add("lose");
+        box.appendChild(resultText);
+        lost = lost + 1;
     } else {
         resultText.classList.add("draw")
         box.appendChild(resultText)
     }
-
 })
-
